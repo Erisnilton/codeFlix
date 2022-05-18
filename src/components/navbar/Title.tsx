7cbbd25578cf7b0d2b8a3aa0937c2727b417fc97
Erisnilton <erisniltongf@gmail.com>
2022-05-18 13:39:41 -0300

    chore: transform logo in link

--- src/components/navbar/Title.tsx
+++ src/components/navbar/Title.tsx
@@ -1,10 +1,12 @@
 import { makeStyles, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import logo from "../../static/img/logo.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     alignItems: "center",
+    cursor: 'pointer'
   },
   logo: {
       [theme.breakpoints.down("sm")]: {
@@ -23,9 +25,11 @@ const useStyles = makeStyles((theme) => ({
 const Title: React.FunctionComponent = () => {
   const classes = useStyles();
   return (
-    <Typography className={classes.root}>
-      <img src={logo} alt="codeFlix"  className={classes.logo}/>
-    </Typography>
+    <Link to={"/"}>
+      <Typography className={classes.root}>
+        <img src={logo} alt="codeFlix"  className={classes.logo}/>
+      </Typography>
+    </Link>
   );
 };
 
